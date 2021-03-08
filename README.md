4a48c8b970b6744d61369556bfc66de5c61a1b5b
chortlesource <chris.m.short@outlook.com>
2021-03-08 16:14:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# TERMNOID - A terminal based tetris clone in C
+
+## Description
+
+This little executable aims to be an efficient and tiny tetris clone that can be played within the terminal window written entirely in C.
+
+
+## Copyright
+
+Copyright (c) 2021 Christopher M. Short
+
+termnoid is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+termnoid is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
---
+++ inc/termnoid.h
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// termnoid - termnoid.h
+//
+// Copyright (c) 2021 Christopher M. Short
+//
+// This file is part of termnoid.
+//
+// termnoid is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// termnoid is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with termnoid. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _TERMNOID_H
+#define _TERMNOID_H
+
+
+/////////////////////////////////////////////////////////////
+// DEPENDENCIES
+//
+
+// Cstd Libs
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Other Libs
+#include <ncurses.h>
+
+#endif // _TERMNOID_H
